4f78bcb22cd8f2a32209f11664341ebae461de0c
RafaelJerezSanchez <rafaeljerezsanchez@gmail.com>
2024-11-22 18:53:04 +0100

    Estilos tarjetas

--- src/paginaWeb/Cards.jsx
+++ src/paginaWeb/Cards.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import './cards.css'; // Importamos el archivo de estilos
+import './cards.css';
 
 function Tarjeta({ titulo, texto, imagen, botonTexto }) {
   return (
